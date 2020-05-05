8cadd04b9f1af331deabca8b38d3ad77b0377715
Girard Vincent <girvincent@gmail.com>
2020-05-05 13:28:19 +0200

    USGDA019 - mise en place barre de navigation #20

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import {Router} from '@angular/router';
+import { Collegue } from '../auth/auth.domains';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +11,24 @@ import {Router} from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  // Mise en place de l'observable pour récupérer le role du collègue, pour l'affichage des onglets de navigation appropriés
+  collegueConnecte: Observable<Collegue>;
+  //collegueConnecte: Collegue;
+
+
+
   constructor(private authSrv: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.collegueConnecte = this.authSrv.collegueConnecteObs;
+    // this.authSrv.collegueConnecteObs.subscribe(
+    //   (value) => {
+    //     this.collegueConnecte = value;
+    //   }, (error) => {
+    //     console.log(error);
+    //   }
+    // );
+    
   }
 
   seDeconnecter() {
