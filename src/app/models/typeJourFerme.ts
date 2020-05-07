a2310b8e7023f6bb9c8d66f54ea5d0ed3e083095
Girard Vincent <girvincent@gmail.com>
2020-05-07 09:55:31 +0200

    mise à jour service #14

---
+++ src/app/models/typeJourFerme.ts
@@ -0,0 +1,5 @@
+export class TypeJourFerme {
+	constructor(public libelle: string) {
+		libelle = libelle;
+	}
+}
---
+++ src/app/service/jour-ferme.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject, Observable } from 'rxjs';
+import { JourFerme } from '../models/jourferme';
+import { TypeJourFerme } from '../models/typeJourFerme';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JourFermeService {
+
+  // Le subject est privé, on le rend "public" avec "abonnementCollegueEnCours()"
+  private jourFermeSubject = new Subject<JourFerme>();
+
+  constructor(private http: HttpClient) { }
+
+  listerJourFerme() {
+    return this.http.get<JourFerme[]>(`http://localhost:8080/jourFerme`);
+  }
+
+  ajouterJourFerme(date: Date, typeJourFerme: TypeJourFerme, commentaire: string): Observable<JourFerme> {
+    return this.http.post<JourFerme>(`http://localhost:8080/jourFerme`,
+      {
+        date: `${date}`,
+        typeJourFerme: `${typeJourFerme}`,
+        commentaire: `${commentaire}`
+      });
+  }
+
+}
