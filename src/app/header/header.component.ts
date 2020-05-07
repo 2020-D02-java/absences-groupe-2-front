2d7cf4267b3142805bab06f67547118f40253fb3
Girard Vincent <girvincent@gmail.com>
2020-05-07 23:28:19 +0200

    Mise en place font awesome + timer sur le header #37

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Router} from '@angular/router';
 import { Collegue } from '../auth/auth.domains';
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../service/auth.service';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faStopwatch } from '@fortawesome/free-solid-svg-icons';
+import 'rxjs/Rx';
+
+
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
+  
+  
   // Mise en place de l'observable pour récupérer le role du collègue, pour l'affichage des onglets de navigation appropriés
   collegueConnecte: Observable<Collegue>;
   utilisateurConnecte: Collegue;
   constructor(private authSrv: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  //** timer debut */
+  iconeStopWatch = faStopwatch;
+  timerSubscription: Subscription;
+  secondes: number = 0;
+  //** timer fin */
+
+  ngOnInit() {
+    //** timer debut ** */
+    const compteur = Observable.interval(1000);
+    this.timerSubscription = compteur.subscribe(
+      (valeur: number) => {
+        this.secondes = valeur;
+      }
+    );
+    //** timer fin ** */
+
     this.collegueConnecte = this.authSrv.collegueConnecteObs;
 
     // On vérifie si l'utilisateur est bien connecté
@@ -32,6 +56,10 @@ export class HeaderComponent implements OnInit {
     this.authSrv.seDeconnecter().subscribe(
       () => this.router.navigate(['/auth'])
     );
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
   }
 
 }
