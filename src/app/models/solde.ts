2854bb9af0238b8e81fb0c7eb2b93281c0469acc
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-05-08 18:45:26 +0200

    reglages conflits

--- src/app/models/solde.ts
+++ src/app/models/solde.ts
@@ -1,5 +1,4 @@
 import { TypeAbsence } from './type-absence';
-import { Collegue } from '../auth/auth.domains';
 
 export class Solde {
   constructor(public nombreDeJours: number, public type: TypeAbsence){
