dacd51f396a961554680b738e965da535d6a0e8a
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-05-13 12:04:23 +0200

    front à jour

--- src/app/absence/visualisation-absence/visualisation-absence.component.ts
+++ src/app/absence/visualisation-absence/visualisation-absence.component.ts
@@ -36,11 +36,11 @@ export class VisualisationAbsenceComponent implements OnInit {
       .subscribe(col => this.collegue = col,
         err => console.log('oops'));
 
-    this.absenceService.listerAbsencesCollegue(this.collegue.email)
+    this.absenceService.listerAbsencesCollegue()
       .subscribe(absences => this.listeAbsences = absences,
         err => console.log('oops'));
 
-    this.absenceService.listerSoldesCollegue(this.collegue.email)
+    this.absenceService.listerSoldesCollegue()
     .subscribe(soldes => this.listeSoldes = soldes,
       err => console.log('oops'));
 
@@ -62,11 +62,11 @@ export class VisualisationAbsenceComponent implements OnInit {
   }
 
   refresh(data) {
-    this.absenceService.listerAbsencesCollegue(this.collegue.email)
+    this.absenceService.listerAbsencesCollegue()
       .subscribe(absences => this.listeAbsences = absences,
         err => console.log('oops'));
 
-    this.absenceService.listerSoldesCollegue(this.collegue.email)
+    this.absenceService.listerSoldesCollegue()
     .subscribe(soldes => this.listeSoldes = soldes,
       err => console.log('oops'));
   }
